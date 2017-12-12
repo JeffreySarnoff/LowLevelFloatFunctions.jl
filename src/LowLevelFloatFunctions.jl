e957daf75e4570004a11ff1d25f27cda1c1b7ef3
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-12-11 23:28:07 -0500

    SysFloats -> SysFloat

--- src/LowLevelFloatFunctions.jl
+++ src/LowLevelFloatFunctions.jl
@@ -12,17 +12,17 @@ import Base.Math: precision, significand_bits, exponent_bits
 
 const SysFloat = Union{Float64, Float32, Float16}
 
-@inline bitwidth(::Type{T}) where T<:SysFloats = sizeof(T) * 8
+@inline bitwidth(::Type{T}) where T<:SysFloat = sizeof(T) * 8
 
 @inline exponent_max(::Type{Float16})  =     15
 @inline exponent_max(::Type{Float32})  =    127
 @inline exponent_max(::Type{Float64})  =   1023
 
-@inline exponent_min(::Type{T}) where T<:SysFloats = 1 - exponent_max(T)
+@inline exponent_min(::Type{T}) where T<:SysFloat = 1 - exponent_max(T)
 
-@inline exponent_bias(::Type{T}) where T<:SysFloats = exponent_max(T)
+@inline exponent_bias(::Type{T}) where T<:SysFloat = exponent_max(T)
 
-@inline exponent_field_max(::Type{T}) where T<:SysFloats = exponent_max(T) + one(convert(Signed, T))
+@inline exponent_field_max(::Type{T}) where T<:SysFloat = exponent_max(T) + one(convert(Signed, T))
 
 include("convert.jl")
 include("fields.jl")
