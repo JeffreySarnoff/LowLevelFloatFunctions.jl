a3f946744d4f2623b90b864db64be1dd3121f9cb
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-12-12 02:16:37 -0500

    signed

--- src/convert.jl
+++ src/convert.jl
@@ -24,3 +24,27 @@
 @inline Base.convert(::Type{Float16}, x::UInt16) = reinterpret(Float16, x)
 @inline Base.convert(::Type{Float32}, x::UInt32) = reinterpret(Float32, x)
 @inline Base.convert(::Type{Float64}, x::UInt64) = reinterpret(Float64, x)
+
+@inline Base.convert(::Type{Unsigned}, ::Type{Float16}) = UInt16
+@inline Base.convert(::Type{Unsigned}, ::Type{Float32}) = UInt32
+@inline Base.convert(::Type{Unsigned}, ::Type{Float64}) = UInt64
+
+@inline Base.convert(::Type{SysFloat}, ::Type{UInt16}) = Float16
+@inline Base.convert(::Type{SysFloat}, ::Type{UInt32}) = Float32
+@inline Base.convert(::Type{SysFloat}, ::Type{UInt64}) = Float64
+
+@inline Base.convert(::Type{Signed}, x::Float16) = reinterpret(Signed, x)
+@inline Base.convert(::Type{Signed}, x::Float32) = reinterpret(Signed, x)
+@inline Base.convert(::Type{Signed}, x::Float64) = reinterpret(signed, x)
+
+@inline Base.convert(::Type{SysFloat}, x::Int16) = reinterpret(Float16, x)
+@inline Base.convert(::Type{SysFloat}, x::Int32) = reinterpret(Float32, x)
+@inline Base.convert(::Type{SysFloat}, x::Int64) = reinterpret(Float64, x)
+
+@inline Base.convert(::Type{Int16}, x::Float16) = reinterpret(Int16, x)
+@inline Base.convert(::Type{Int32}, x::Float32) = reinterpret(Int32, x)
+@inline Base.convert(::Type{Int64}, x::Float64) = reinterpret(Int64, x)
+
+@inline Base.convert(::Type{Float16}, x::Int16) = reinterpret(Float16, x)
+@inline Base.convert(::Type{Float32}, x::Int32) = reinterpret(Float32, x)
+@inline Base.convert(::Type{Float64}, x::Int64) = reinterpret(Float64, x)
