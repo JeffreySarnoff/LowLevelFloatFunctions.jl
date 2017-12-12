86d5b4e1669c170908d11c30aaf6555127b138ce
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-12-12 05:02:00 -0500

    biased unbiased exponent_field

--- src/getset_fields.jl
+++ src/getset_fields.jl
@@ -7,7 +7,13 @@
 @inline sign_and_exponent_fields(x::T) where T<:Unsigned = isolate_sign_and_exponent_fields(x) >> exponent_field_offset(T)
 @inline exponent_and_significand_fields(x::T) where T<:Unsigned = isolate_exponent_and_significand_fields(x) >> significand_field_offset(T)
 
-for F in (:sign_field, :exponent_field, :significand_field, :sign_and_exponent_fields, :exponent_and_significand_fields)
+
+@inline biased_exponent_field(x::T) where T<:SysFloat = exponent_field(x)
+@inline unbiased_exponent_field(x::T) where T<:SysFloat = exponent_field(x) - convert(Unsigned, exponent_bias(T))
+
+for F in (:sign_field, :exponent_field, :significand_field,
+          :biased_exponent_field, :unbaised_exponent_field,
+          :sign_and_exponent_fields, :exponent_and_significand_fields)
   @eval begin
     @inline $F(x::T) where T<:SysFloat = $F(convert(Unsigned,x))
   end
@@ -19,7 +25,9 @@ for (F,C,P) in ((:sign_field, :clear_sign_field, :prepare_sign_field),
                 (:exponent_field, :clear_exponent_field, :prepare_exponent_field),
                 (:significand_field, :clear_significand_field, :prepare_significand_field),
                 (:sign_and_exponent_fields, :clear_sign_and_exponent_fields, :prepare_sign_and_exponent_fields),
-                (:exponent_and_significand_fields, :clear_exponent_and_significand_fields, :prepare_exponent_and_significand_fields))
+                (:exponent_and_significand_fields, :clear_exponent_and_significand_fields, :prepare_exponent_and_significand_fields)
+                (:biased_exponent_field, :clear_exponent_field, :prepare_exponent_field),
+                (:unbiased_exponent_field, :clear_exponent_field, :prepare_exponent_field))
   for (T,U,S) in ((:Float64, :UInt64, :Int64), (:Float32, :UInt32, :Int32), (:Float16, :UInt16, :Int32))
     @eval begin
         @inline $F(x::$T, y::$U) = reinterpret($T, $C(x) | $P(y))
@@ -50,3 +58,5 @@ end
 @inline prepare_significand_field(x::T) where T<:Unsigned = (x & significand_field_mask_lsbs(T)) << significand_field_offset(T)
 @inline prepare_sign_and_exponent_fields(x::T) where T<:Unsigned = (x & sign_and_exponent_fields_mask_lsbs(T)) << exponent_field_offset(T)
 @inline prepare_exponent_and_significand_fields(x::T) where T<:Unsigned = (x & exponent_and_significand_fields_mask_lsbs(T)) << exponent_and_significand_fields_offset(T)
+@inline prepare_biased_exponent_field(x::T) where T<:Unsigned = prepare_exponent_field(x)
+@inline prepare_unbiased_exponent_field(x::T) where T<:Unsigned = prepare_exponent_field(x)
