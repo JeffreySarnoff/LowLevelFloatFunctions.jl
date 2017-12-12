346d715dcd06d35f7b9630adc4a5af82297c6b54
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-12-12 01:57:12 -0500

    add specifics

--- src/convert.jl
+++ src/convert.jl
@@ -16,3 +16,11 @@
 @inline Base.convert(::Type{SysFloat}, x::UInt16) = reinterpret(Float16, x)
 @inline Base.convert(::Type{SysFloat}, x::UInt32) = reinterpret(Float32, x)
 @inline Base.convert(::Type{SysFloat}, x::UInt64) = reinterpret(Float64, x)
+
+@inline Base.convert(::Type{UInt16}, x::Float16) = reinterpret(UInt16, x)
+@inline Base.convert(::Type{UInt32}, x::Float32) = reinterpret(UInt32, x)
+@inline Base.convert(::Type{UInt64}, x::Float64) = reinterpret(UInt64, x)
+
+@inline Base.convert(::Type{Float16}, x::UInt16) = reinterpret(Float16, x)
+@inline Base.convert(::Type{Float32}, x::UInt32) = reinterpret(Float32, x)
+@inline Base.convert(::Type{Float64}, x::UInt64) = reinterpret(Float64, x)
