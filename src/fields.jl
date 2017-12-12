353726ca7f2e1c3b49ee87fe5be061187028b4c9
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2017-12-12 03:34:11 -0500

    cleanup set

--- src/fields.jl
+++ src/fields.jl
@@ -75,8 +75,8 @@ for (F,C,P) in ((:sign_field, :clear_sign_field, :prepare_sign_field),
                 (:exponent_and_significand_fields, :clear_exponent_and_significand_fields, :prepare_exponent_and_significand_fields))
   for (T,U,S) in ((:Float64, :UInt64, :Int64), (:Float32, :UInt32, :Int32), (:Float16, :UInt16, :Int32))
     @eval begin
-        @inline $F(x::$T, y::$U) = convert($T, $C(convert($U, x)) | $P(y))
-        @inline $F(x::$T, y::$S) = convert($T, $C(convert($U, x)) | $P(reinterpret($U, y)))
+        @inline $F(x::$T, y::$U) = convert($T, $C(reinterpret($U, x)) | $P(y))
+        @inline $F(x::$T, y::U) where U<:Unsigned = convert($T, $C(reinterpret($U, x)) | $P(convert($U, y)))
     end
   end
 end
